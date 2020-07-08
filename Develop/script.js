39bf9735c96be6f845e9fdfb099f09b7d65d7044
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-07-07 21:41:22 -0400

    added arrays in js

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,18 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+// Number of Characters Slider
+const passwordLengthSlider = document.getElementById('numberOfCharacterInputslider');
+const passwordLengthNumber = document.getElementById('numberOfCharacterInputDigit')
+
+function sliderChange(val) {
+  document.getElementById('sliderStatus').innerHTML = val;
+}
+
+// All password content arrays
+const lowerCaseChar = "abcdefghijklmnopqrstuvwxyz".split('');
+const upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+const numChar = "0123456789".split('');
+const specialChar = "`~!@#$%^&*()-_=+[]{}\|;:',./?<>".split("");
 
 // Write password to the #password input
 function writePassword() {

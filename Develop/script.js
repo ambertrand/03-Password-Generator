e4307bdaa98d2ebf5e492860b1f23480a20dd912
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-07-09 12:51:10 -0400

    added checkbox confirmation

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,6 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
-// Links up with number input from HTML  
-const passwordLength = document.getElementById("passwordLength");
-const passwordNumber = document.getElementById("passwordNumber");
-
 // All password content arrays
 const lowerCaseChar = "abcdefghijklmnopqrstuvwxyz".split('');
 const upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
@@ -12,8 +8,11 @@ const numChar = "0123456789".split('');
 const specialChar = "`~!@#$%^&*()-_=+[]{}\|;:',./?<>".split("");
 
 
-// Syncs number input & character slider to match when mouse moves slider
-// Source link https://www.youtube.com/watch?v=iKo9pDKKHnc
+// Syncs number input & character slider to match when mouse moves slider. Source link https://www.youtube.com/watch?v=iKo9pDKKHnc
+// Links up with number input from HTML  
+const passwordLength = document.getElementById("passwordLength");
+const passwordNumber = document.getElementById("passwordNumber");
+
 passwordLength.addEventListener('input', syncCharNum);
 passwordNumber.addEventListener('input', syncCharNum);
 
@@ -24,18 +23,21 @@ function syncCharNum(n) {
   passwordLength.value = value
 }
 
+// Confirming what checkboxes are selected
+const lowerCaseChar = document.getElementById("#lowerCaseChar").checked;
+const upperCaseChar = document.getElementById("#upperCaseChar").checked;
+const numbers = document.getElementById("#numbers").checked;
+const specialChar = document.getElementById("#specialChar").checked;
+
+
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

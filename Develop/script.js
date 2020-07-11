0bcc4e1a72df62f461720f1911331d601ee93cc9
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-07-11 00:13:41 -0400

    fixed slider value length

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,40 @@
 // Syncs number input & character slider to match when mouse moves slider. Source link https://www.youtube.com/watch?v=iKo9pDKKHnc
 // Links up with number input from HTML  
-const passwordLength = document.getElementById('passwordLength');
-const passwordNumber = document.getElementById('passwordNumber');
+const passwordLength = document.querySelector("#passwordLength");
+const pwlengthEl = document.querySelector("#passwordNumber");
 
 passwordLength.addEventListener('input', syncCharNum);
-passwordNumber.addEventListener('input', syncCharNum);
+pwlengthEl.addEventListener('input', syncCharNum);
 
-// pwLength = document.getElementById("passwordNumber").value;
-// document.getElementById("passwordNumber").innerHTML = " " + pwLength;
-
-// let passwordLength = document.getElementById("pwLength").value;
 
 // Calls the event function when the slider is moved to sync the characters and numbers
 function syncCharNum(n) {
   const value = n.target.value;
-  passwordNumber.value = value;
+  pwlengthEl.value = value;
   passwordLength.value = value;
+  console.log(value);
+
 }
 
 const resultEl = document.querySelector("#password");
-const pwlengthEl = document.querySelector("#passwordNumber");
 const lowerCaseCharEl = document.querySelector("#lowerCaseChar");
 const upperCaseCharEl = document.querySelector("#upperCaseChar");
 const numbersEl = document.querySelector("#numbers");
 const specialCharEl = document.querySelector("#specialChar");
 const generateEl = document.querySelector("#generate");
+
+// Generate Event listener
+generateEl.addEventListener('click', function() {
+  let length = parseInt(pwlengthEl.value);
+  const containsLower = lowerCaseCharEl.checked;
+  const containsUpper = upperCaseCharEl.checked;
+  const containsNumber = numbersEl.checked;
+  const containsSpecial = specialCharEl.checked;
+
+// console.log(length);
+
+  resultEl.innerText = generatePassword(containsLower, containsUpper, containsNumber, containsSpecial, length);
+});
 
 // Moving each function into an object
 // Source https://www.youtube.com/watch?v=duNmhKgtcsI

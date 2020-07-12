692e3acd4624476e5f409c0df29148b80bb1562f
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-07-12 11:13:54 -0400

    Debugged event listeners

--- Develop/script.js
+++ Develop/script.js
@@ -12,10 +12,12 @@ function syncCharNum(n) {
   const value = n.target.value;
   pwlengthEl.value = value;
   passwordLength.value = value;
-  console.log(value);
+
+  // console.log(value);
 
 }
 
+// Selecting ID from HTML
 const resultEl = document.querySelector("#password");
 const lowerCaseCharEl = document.querySelector("#lowerCaseChar");
 const upperCaseCharEl = document.querySelector("#upperCaseChar");
@@ -25,59 +27,36 @@ const generateEl = document.querySelector("#generate");
 
 
 
-// Generate Event listener
-generateEl.addEventListener('click', function () {
+generateEl.addEventListener("click", function () {
   let length = parseInt(pwlengthEl.value);
-  const containsLower = lowerCaseCharEl.checked;
-  const containsUpper = upperCaseCharEl.checked;
-  const containsNumber = numbersEl.checked;
-  const containsSpecial = specialCharEl.checked;
-
+  let generatedPassword = [];
+  const special = '!@#$%^&*(){}[]=<>/,.';
   // console.log(length);
-
-  resultEl.innerText = generatePassword(containsLower, containsUpper, containsNumber, containsSpecial, length);
+  if ((length < 8) || (length > 128)) {
+    alert("Password needs to be greater than 8 and less than 128 characters");
+  }
+  if (lowerCaseCharEl.checked === true) {
+    for (let i = 0; i < 26; i++) {
+      generatedPassword.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97));
+  }
+}
+  if (upperCaseCharEl.checked === true) {
+    for (let i = 0; i < 26; i++) {
+       generatedPassword.push(String.fromCharCode(Math.floor(Math.random() * 26) + 65));
+      }
+  }
+  if (numbersEl.checked === true) {
+    for (let i = 0; i < 10; i++) {
+       generatedPassword.push(String.fromCharCode(Math.floor(Math.random() * 10) + 48));
+        }
+      }
+  if (specialCharEl.checked === true) {
+       for (let i = 0; i < special.length; i++) {
+        generatedPassword.push(special[Math.floor(Math.random() * special.length)]);
+    }
+  }
 });
 
-// Generate password function
-function generatePassword(lower, upper, number, symbol, length) {
-
-  // Initiates password variable
-  let generatePassword = '';
-
-  const totalPassword = lower + upper + number + symbol;
-
-  // console.log('totalPassword', totalPassword)
-
-  // Filters out unchecked boxes and creates array of objects
-  const totalArr = [{ lower }, { upper }, { number }, { symbol }].filter((item) => Object.values(item)[0]
-      // function(item) {(Object.values(item)[0])}
-    );
-
-  // console.log('totalArr: ', totalArr);
-
-  // Checking if no boxes are checked
-  if (totalPassword === 0) {
-    return '';
-  }
-
-  //For loop for the password length
-  for (let i = 0; i < length; i += totalPassword) {
-    totalArr.forEach(type => {
-      const funcName = Object.keys(type)[0];
-      // console.log('funcName: ', funcName);
-
-      // Taking password string and append on to it to take upper, lower, number, symbol
-      generatePassword += charObject[funcName]();
-      //     resultEl.value = password;
-    });
-  }
-
-  const finalPassword =
-  console.log(generatePassword);
-}
-
-// Reverse filter and arrow function line 52 
-// Explaining loop line 64
 // Pushing lowercase, upper, number etc. function into array
 // Hit for loop, inside randomly selected element in array
 // Push string onto page as password
@@ -85,27 +64,28 @@ function generatePassword(lower, upper, number, symbol, length) {
 
 // Moving each function into an object
 // Source https://www.youtube.com/watch?v=duNmhKgtcsI
-const charObject = {
-  lower: lowerCaseChar,
-  upper: upperCaseChar,
-  number: numChar,
-  symbol: specialChar,
-};
+// const charObject = {
+//   lower: lowerCaseChar,
+//   upper: upperCaseChar,
+//   number: numChar,
+//   symbol: specialChar,
+// };
+
 
 // Generator Functions using ascii characters for lower, upper & numbers
-function lowerCaseChar() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+// function lowerCaseChar() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
 
-function upperCaseChar() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+// function upperCaseChar() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
 
-function numChar() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+// function numChar() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
 
-function specialChar() {
-  const special = '!@#$%^&*(){}[]=<>/,.';
-  return special[Math.floor(Math.random() * special.length)];
-}
+// function specialChar() {
+//   const special = '!@#$%^&*(){}[]=<>/,.';
+//   return special[Math.floor(Math.random() * special.length)];
+// }
